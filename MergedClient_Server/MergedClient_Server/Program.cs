0b1e9cbe779255ef86ca0bf575961f9665b78940
david <david@DESKTOP-5G5AQFQ>
2017-02-28 14:51:42 -0500

    Ajoutez des fichiers projet.

---
+++ MergedClient_Server/MergedClient_Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AtelierXNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Atelier game = new Atelier())
+            {
+                game.Run();
+            }
+        }
+    }
+}
---
+++ RelayServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RelayServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Try to start a new server using the default port in the config file.
+            try
+            {
+                Server server = new Server(Properties.Settings.Default.Port);
+
+                while (true) { Thread.Sleep(1000); }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
